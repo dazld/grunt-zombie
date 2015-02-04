2fd343613ada76dc1c834bfd1e3cab99b667728d
Dan Peddle <dan.peddle@gmail.com>
2015-02-04 11:47:02 +0100

    space not tabs

--- tasks/zombie.js
+++ tasks/zombie.js
@@ -10,35 +10,35 @@
 
 module.exports = function(grunt) {
 
-	// Please see the Grunt documentation for more information regarding task
-	// creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-	grunt.task.registerTask('zombie', 'Run Zombie JS tests from Grunt', function() {
-		var done = grunt.task.current.async();
-		if (!this.options().targetfiles) {
-			grunt.fail.warn('You must specify a test file.');
-		}
-		var target = this.options().targetfiles;
-		if (!grunt.file.exists(target)) {
-			grunt.log.warn('Source file "' + target + '" not found.');
-			return;
-		}
+    grunt.task.registerTask('zombie', 'Run Zombie JS tests from Grunt', function() {
+        var done = grunt.task.current.async();
+        if (!this.options().targetfiles) {
+            grunt.fail.warn('You must specify a test file.');
+        }
+        var target = this.options().targetfiles;
+        if (!grunt.file.exists(target)) {
+            grunt.log.warn('Source file "' + target + '" not found.');
+            return;
+        }
 
 
-		var child_proc = grunt.util.spawn({
-			cmd: "node",
-			args: [target]
-		}, function(error, result, code) {
+        var child_proc = grunt.util.spawn({
+            cmd: "node",
+            args: [target]
+        }, function(error, result, code) {
 
-			if (error) {
-				grunt.log.error('Unspecified Error:', error.message);
-				done(false);
-			}
-			if (code == 0) {
-				grunt.log.oklns("Test Completed successfully");
-				done(true);
-			}
-		});
+            if (error) {
+                grunt.log.error('Unspecified Error:', error.message);
+                done(false);
+            }
+            if (code == 0) {
+                grunt.log.oklns("Test Completed successfully");
+                done(true);
+            }
+        });
 
-	});
+    });
 };
